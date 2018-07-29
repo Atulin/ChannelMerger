9f057cce1205ca614781002599ecf439f63942ba
Angius <koumarin@gmail.com>
2018-07-29 07:09:07 +0200

    Initial commit

---
+++ ChannelMerger/MainWindow.xaml.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Forms;
+using ImageMagick;
+using MahApps.Metro.Controls;
+using MahApps.Metro.Controls.Dialogs;
+using Button = System.Windows.Controls.Button;
+using OpenFileDialog = Microsoft.Win32.OpenFileDialog;
+
+
+namespace ChannelMerger
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        private readonly string[] _channelPaths = new string[4];
+        private readonly MagickImageCollection _channelImages = new MagickImageCollection();
+        private string _outputPath;
+        private readonly MagickReadSettings _settings = new MagickReadSettings();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+            // Tells the xc: reader the image to create should be 800x600
+            _settings.Width = 800;
+            _settings.Height = 600;
+        }
+
+        private async void ChannelBtn_Click(object sender, RoutedEventArgs e)
+        {
+            string tag = ((Button) sender).Tag.ToString();
+
+            OpenFileDialog file = new OpenFileDialog
+            {
+                Filter = Helpers.ImagesFilter()
+            };
+            file.ShowDialog();
+
+            if (!String.IsNullOrEmpty(file.FileName))
+            {
+                MagickImage img = new MagickImage(file.FileName);
+                if (!img.CheckGrayscale())
+                {
+                    await this.ShowMessageAsync("Error!", "This image isn't grayscale!");
+                    return;
+                }
+
+            }
+
+            switch (tag.ToLower())
+            {
+                case "r":
+                    _channelPaths[0] = file.FileName;
+                    redPath.Text = file.FileName;
+                    break;
+
+                case "g":
+                    _channelPaths[1] = file.FileName;
+                    greenPath.Text = file.FileName;
+                    break;
+
+                case "b":
+                    _channelPaths[2] = file.FileName;
+                    bluePath.Text = file.FileName;
+                    break;
+
+                case "a":
+                    _channelPaths[3] = file.FileName;
+                    alphaPath.Text = file.FileName;
+                    break;
+
+                default:
+                    throw new ArgumentException("How? Just... how?");
+            }
+        }
+
+        private void MergeBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (String.IsNullOrEmpty(_outputPath)) return;
+
+            foreach (string path in _channelPaths)
+            {
+                _channelImages.Add(String.IsNullOrEmpty(path)
+                    ? new MagickImage("xc:white", _settings)
+                    : new MagickImage(path));
+            }
+
+            _channelImages.MergeChannels(outputPath.Text, fileName.Text);
+            _channelImages.Clear();
+        }
+
+        private void OutputBtn_Click(object sender, RoutedEventArgs e)
+        {
+            using (var folder = new FolderBrowserDialog())
+            {
+                if (!string.IsNullOrEmpty(_outputPath))
+                {
+                    folder.SelectedPath = _outputPath;
+                }
+                folder.ShowDialog();
+                _outputPath = folder.SelectedPath;
+            }
+            outputPath.Text = _outputPath;
+        }
+    }
+}
---
+++ ChannelMerger/helpers.cs
@@ -0,0 +1,50 @@
+﻿using System.Diagnostics;
+using System.Drawing.Imaging;
+using System.Globalization;
+using System.Linq;
+using ImageMagick;
+
+namespace ChannelMerger
+{
+    static class Helpers
+    {
+        public static string ImagesFilter()
+        {
+            ImageCodecInfo[] codecs = ImageCodecInfo.GetImageEncoders();
+            string filter = string.Format("{0}|All image files ({1})|{1}|All files|*", 
+                string.Join("|", codecs.Select(codec => 
+                    string.Format("{0} ({1})|{1}", codec.CodecName.Split(' ')[1], codec.FilenameExtension)).ToArray()),
+                string.Join(";", codecs.Select(codec => codec.FilenameExtension).ToArray()));
+
+            return filter;
+        }
+
+        public static MagickImage MergeChannels(this MagickImageCollection images, string path, string name)
+        {
+            IMagickImage result = images.Combine();
+            result.Write(path + "/" + name);
+            Process.Start(result.FileName);
+            var img = new MagickImage(result.FileName);
+            return img;
+        }
+
+        public static bool CheckGrayscale(this MagickImage i)
+        {
+            using (MagickImage image = i)
+            {
+                // -colorspace HSL
+                image.ColorSpace = ColorSpace.HSL;
+
+                // -channel g -separate +channel
+                string r;
+                using (IMagickImage channel = image.Separate(Channels.Gray).First())
+                {
+                    // -format "%[fx:mean]"
+                    r = channel.FormatExpression("%[fx:mean]");
+                }
+
+                return double.Parse(r, NumberStyles.Any, CultureInfo.InvariantCulture) < 1e-10;
+            }
+        }
+    }
+}
